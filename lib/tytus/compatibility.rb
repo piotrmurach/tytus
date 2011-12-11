3e94e10a549d9406d2e35aef9fbf1ca4457f7565
Piotr Murach <pmurach@gmail.com>
2011-12-11 22:38:27 +0000

    Add check for rails gem existance.

--- lib/tytus/compatibility.rb
+++ lib/tytus/compatibility.rb
@@ -1,19 +1,21 @@
 # encoding: utf-8
 
-if Rails.version.to_i < 3
-  def rails_3
-    false
-  end
+if defined? Rails
+  if Rails.version.to_i < 3
+    def rails_3
+      false
+    end
 
-  def rails_2
-    yield
-  end
-else
-  def rails_3
-    yield
-  end
+    def rails_2
+      yield
+    end
+  else
+    def rails_3
+      yield
+    end
 
-  def rails_2
-    false
+    def rails_2
+      false
+    end
   end
 end
