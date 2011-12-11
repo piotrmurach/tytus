3d802f35fc39af89f495714fcca71394b17bcebd
Piotr Murach <pmurach@gmail.com>
2011-12-11 13:19:01 +0000

    Initialize rails hooks.

--- lib/tytus.rb
+++ lib/tytus.rb
@@ -1,8 +1,15 @@
 # encoding: utf-8
 
 require "tytus/version"
+require "tytus/compatibility"
 require "tytus/controller_extensions"
+require "tytus/view_extensions"
 
 module Tytus
-
+  if defined? Rails::Railtie
+    require "tytus/railtie"
+  else
+    Tytus::Railtie.insert_view
+    Tytus::Railtie.insert_controller
+  end
 end # Tytus
