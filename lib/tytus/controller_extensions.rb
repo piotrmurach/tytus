486a1ae1613f883c67fec041c169d6b92d6f3ece
Piotr Murach <pmurach@gmail.com>
2011-12-11 19:33:18 +0000

    Add ability to manage titles in controller.

--- lib/tytus/controller_extensions.rb
+++ lib/tytus/controller_extensions.rb
@@ -3,5 +3,41 @@
 module Tytus
   module ControllerExtensions
 
+    def self.included(base)
+      base.extend ClassMethods
+      if base.respond_to? :class_inheritable_accessor
+        base.class_inheritable_accessor :_page_title
+      else
+        base.superclass_delegating_accessor :_page_title
+      end
+      base.class_eval do
+        include InstanceMethods
+      end
+    end
+
+    module ClassMethods
+
+      # Setup before filter which loads title for all actions.
+      #
+      def title(*args)
+        options = args.extract_options!
+        text = args
+        before_filter(options) do |instance|
+          instance.send(:title, text)
+        end
+      end
+
+    end # ClassMethods
+
+    module InstanceMethods
+
+      # Setup title for the current resource
+      #
+      def title(text)
+        self.class._page_title = text
+      end
+
+    end # InstanceMethods
+
   end # ControllerExtensions
 end # Tytus
