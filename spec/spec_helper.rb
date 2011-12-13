4259afdf940376dafb1235fb885bdfb07f6a58f3
Piotr Murach <pmurach@gmail.com>
2011-12-13 23:22:24 +0000

    Add spec rails integration setup.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,1 +1,32 @@
 require 'tytus'
+
+# Configure Rails Envinronment
+ENV["RAILS_ENV"] = "test"
+
+require File.expand_path("../rails_app/config/environment.rb",  __FILE__)
+require "rails/test_help"
+
+require 'action_controller'
+require 'action_view'
+
+ActionMailer::Base.delivery_method = :test
+ActionMailer::Base.perform_deliveries = true
+ActionMailer::Base.default_url_options[:host] = "test.com"
+
+Rails.backtrace_cleaner.remove_silencers!
+
+RSpec.configure do |config|
+  config.include ActionController
+  config.include ActionView
+end
+
+# Configure capybara for integration testing
+require "capybara/rails"
+Capybara.default_driver   = :rack_test
+Capybara.default_selector = :css
+
+# Run any available migration
+ActiveRecord::Migrator.migrate File.expand_path("rails_app/db/migrate/", __FILE__)
+
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
