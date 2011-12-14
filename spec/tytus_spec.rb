13c2bdaba8a49426621f95d303e3f4c9af64a9e3
Piotr Murach <pmurach@gmail.com>
2011-12-14 22:49:18 +0000

    Add specs for view methods.

--- spec/tytus_spec.rb
+++ spec/tytus_spec.rb
@@ -11,6 +11,10 @@ describe Tytus do
 
   context "methods for classes extending ControllerExtensions" do
 
+    it "should be mixed in into ActionController::Base" do
+      controller.included_modules.include?(Tytus::ControllerExtensions).should be_true
+    end
+
     context '.title' do
       it "shoulbe be available to ActionController::Base" do
         controller.should respond_to :title
@@ -25,21 +29,51 @@ describe Tytus do
 
   context "methods for classes extending ViewExtensions" do
 
-    context ".render_page_title" do
-      it "should be available to ActionView::Base" do
+    it "should be mixed in into ActionView::Base" do
+      view.included_modules.include?(Tytus::ViewExtensions).should be_true
+    end
+
+    context '.site_name' do
+      it "should be avaialbe to view" do
+        view_instance.should respond_to :site_name
+      end
+
+      it "should use translation if name not present" do
+        view_instance.site_name.should eq 'My Site Name'
+      end
+
+      it "should use name parameter if given" do
+        view_instance.site_name('my-site').should eq 'my-site'
+      end
+    end
+
+    context '.render_page_title' do
+      it 'should be available to view' do
         view_instance.should respond_to :render_page_title
       end
     end
 
     context '.check_translation' do
       it 'should find translation for controller-name' do
-        view_instance.should_receive(:translate).and_return 'controller-name'
-        view_instance.check_translation('controller-name')
+        view_instance.check_translation('projects').should eq 'All Projects'
       end
 
       it 'should fail to find translation and return humanized controller name' do
-        view_instance.stub(:translate).and_return nil
         view_instance.check_translation('SomeController').should eq 'Somecontroller'
+      end
+    end
+
+    context '.title' do
+      it 'should be available to view' do
+        view_instance.should respond_to :title
+      end
+
+      it "should " do
+        pending
+        controller_instance.stub(:class).and_return controller
+        controller.stub(:name).and_return 'ArticlesController'
+        view_instance.title 'some', 'view'
+        controller._page_title.should eq 'some :: view'
       end
     end
   end # ViewExtensions
