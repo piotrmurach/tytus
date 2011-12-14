f32108445bf32ee21d7fa1139d46703a3f4387aa
Piotr Murach <pmurach@gmail.com>
2011-12-14 21:35:13 +0000

    Vendor rails app for view testing.

---
+++ spec/rails_app/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsApp::Application.config.secret_token = '812ca3d4fc05514bdf140a44bbb92e9bcb98b4821a4ee24480a21c2b709d22c968ae50b99ad89dc69cabd012c0a158b1e1af559eafc709ab210b68302125425e'
---
+++ spec/rails_app/config/routes.rb
@@ -0,0 +1,4 @@
+RailsApp::Application.routes.draw do
+  root :to => 'articles#index'
+  resources :articles
+end
