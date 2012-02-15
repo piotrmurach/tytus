3be3aa6b0cf63ad969505b6bc29370d65faf1413
Piotr Murach <pmurach@gmail.com>
2012-02-15 11:45:12 +0000

    Move all capybara setup inside spec support, remove migrations setup, move support load.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -9,6 +9,9 @@ require "rspec/rails"
 require 'action_controller'
 require 'action_view'
 
+# Load support files
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
 ActionMailer::Base.delivery_method = :test
 ActionMailer::Base.perform_deliveries = true
 ActionMailer::Base.default_url_options[:host] = "test.com"
@@ -19,14 +22,3 @@ RSpec.configure do |config|
   config.include ActionController
   config.include ActionView
 end
-
-# Configure capybara for integration testing
-require "capybara/rails"
-Capybara.default_driver   = :rack_test
-Capybara.default_selector = :css
-
-# Run any available migration
-ActiveRecord::Migrator.migrate File.expand_path("rails_app/db/migrate/", __FILE__)
-
-# Load support files
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -4,3 +4,6 @@ require 'capybara/dsl'
 RSpec.configure do |config|
   config.include Capybara::DSL, :example_group => { :file_path => /\bspec\// }
 end
+
+Capybara.default_driver   = :rack_test
+Capybara.default_selector = :css
